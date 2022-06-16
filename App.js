5d71d7078b58a4b23e4ae99f9f6282c0c097e185
Elodie M <mongellaz.e@gmail.com>
2022-06-16 10:13:15 +0200

    check mark

--- App.js
+++ App.js
@@ -38,6 +38,15 @@ function deleteCheck(e){
    //DELETE TODO
    if(item.classList[0] === "trash-btn"){
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+        todo.remove();
+    });
+   }
+
+   //CHECK MARK
+   if(item.classList[0] === "complete-btn"){
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
    }
 }
